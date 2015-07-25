e4ce04d2da9097957b9b444c2dfb3a48defe421d
psicorafa <psicorafa@hotmail.com>
2015-07-25 19:25:50 +0200

    quiz10

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,63 @@
 var models = require('../models/models.js');
 
+// Autoload -factoriza el código si ruta incluye : quizId
+exports.load = function(req, res, next, quizId) {
+models.Quiz.find(QuizId).then(
+	function(quiz){
+		if (quiz){
+			req.quiz = quiz;
+			next();
+		} else { next(new Error('No existe QuizId=' + quizId)); }
+	}
+	).catch(function(error) { next(error);});
+
+};
+
+// GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index', { quizes: quizes});	
+	}
+	).catch(function(error){ next(error);})
+};
+
+// GET /quizes/:id
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', { quiz: req.quiz});
+	})
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res){
+	var resultado = 'Incorrecto';
+		
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	} 
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
 
 // GET /quizes/question
-exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question', { pregunta: quiz[0].pregunta})
-	})
-};
+//exports.question = function(req, res){
+//	models.Quiz.findAll().success(function(quiz){
+//		res.render('quizes/question', { pregunta: quiz[0].pregunta})
+//	})
+//};
+
+
 
 // GET /quizes/answer
-exports.answer = function(req, res){
-	models.Quiz.findAll().success(function(quiz){	
-	if (req.query.respuesta === quiz[0].respuesta){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
-	})
-};
+//exports.answer = function(req, res){
+//	models.Quiz.findAll().success(function(quiz){	
+//	if (req.query.respuesta === quiz[0].respuesta){
+//		res.render('quizes/answer', {respuesta: 'Correcto'});
+//	} else {
+//		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+//	}
+//	})
+//};
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload de comandos con Quiz:Id
+router.param('quizId', quizController.load); // autoload:quizId
+
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+
+router.get('/quizes',			quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/authors', function(req, res) {
 	res.render('authors');
